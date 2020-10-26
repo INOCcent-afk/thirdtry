08c9201f174fce945262492669b2ea9c369d5f21
INOCcent-afk <michael828inoc@gmail.com>
2020-10-26 12:00:51 +0800

    Edit router

--- src/App.js
+++ src/App.js
@@ -9,17 +9,15 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/product" exact component={Product} />
-          <Route path="/services" component={Services} />
-        </Switch>
-        <Footer />
-      </div>
-    </Router>
+    <div className="App">
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/product" exact component={Product} />
+        <Route path="/services" component={Services} />
+      </Switch>
+      <Footer />
+    </div>
   );
 }
 
